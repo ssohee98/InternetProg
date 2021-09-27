a08722e3f0c484f5be92f2d94d50d4c4ef06eed8
So-Hee Jeong <jsohee98@naver.com>
2021-09-27 20:39:05 +0900

    CBV로 블로그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),    # 서버IP/blog
-    path('<int:pk>/', views.single_post_page)    # 서버IP/blog/
+    # path('', views.index),    # 서버IP/blog
+    # path('<int:pk>/', views.single_post_page)    # 서버IP/blog/
+
+    path('<int:pk>/', views.PostDetail.as_view()),  # 뷰가 부르는 부분을 함수가 아니라 뷰 같은 클래스로
+    path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')  # 최근 생성된 순서대로 모두 출력
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')  # 최근 생성된 순서대로 모두 출력
+#
+#     return render(request, 'blog/post_list.html',  # 사용할 템플릿
+#                   {
+#                       'posts': posts
+#                   }
+#                   )
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)  # pk 값만 가져오기
+#
+#     return render(request, 'blog/post_detail.html',  # 사용할 템플릿
+#                   {
+#                       'post': post
+#                   }
+#                   )
 
-    return render(request, 'blog/index.html',  # 사용할 템플릿
-                  {
-                      'posts': posts
-                  }
-                  )
+class PostList(ListView) :
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/post_list.html'
+    # post_list.html
+    # 따로 템플릿 연결을 하지 않아도 클래스에 해당하는 html 이름으로 바꾸었으므로 자동으로 연결된다.
+    
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)  # pk 값만 가져오기
-
-    return render(request, 'blog/single_post_page.html',  # 사용할 템플릿
-                  {
-                      'post': post
-                  }
-                  )
-
+class PostDetail(DetailView) :
+    model = Post
+    # template_name = 'blog/post_detail.html'
+    # post_detail.html
 
 
