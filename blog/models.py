c6e315cbf8d2528841b619ce8d65a3926c994559
So-Hee Jeong <jsohee98@naver.com>
2021-10-10 20:33:43 +0900

    미리보기와 요약문 추가

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+import os
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     #사용한 이미지 주소 (업로드할 경로, 필수는 아님)
@@ -23,3 +26,11 @@ class Post(models.Model):
     # 각 상세 페이지로 이동 + 장고에 view on site 버튼
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+
+    # 업로드된 파일 이름 가져오기
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    # 업로드된 파일 확장자 가져오기
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]  # split 된 배열중 가장 마지막 요소가 확장자
